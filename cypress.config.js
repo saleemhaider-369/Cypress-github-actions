d680fd55ed6d7f7c9e098f12d68947c12d15a386
saleem-haider <maliksaleem369369@gmail.com>
2025-08-26 15:56:56 +0500

    upload allure report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("@shelex/cypress-allure-plugin/writer")(on, config);
+      return config;
     },
+  },
+  env: {
+    allure: true,
   },
 });
